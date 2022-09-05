6c8ca8e5b5e713b7bd770855d04a1d6dcc89ab3c
luni64 <12611497+luni64@users.noreply.github.com>
2022-09-05 13:29:12 +0200

    refactoring

--- src/CallbackHelper.h
+++ src/CallbackHelper.h
@@ -3,6 +3,7 @@
 #include "callback.h"
 #include <memory>
 
+
 /**
  * To avoid dynamic memory allocation we need to preallocate static memory and construct the objects into this using 'placement new'
  * Memory usage:
@@ -20,17 +21,16 @@ class CallbackHelper
     // generates a callback from a function pointer
     callback_t* makeCallback(void (*fptr)(), unsigned slot)
     {
-        return new (memoryPool + slot * slotSize) Callback_fptr(fptr); // please note that placement new does not allocate memory!
+        return new (memoryPool + slot * slotSize) Callback_fptr(fptr); // please note that placement new does NOT allocate memory!
     }
 
     // tries to generate a callback from anything with an operator()() const. I.e., lambdas, functors ...
     template<typename T>
-    callback_t* makeCallback(T&& l, unsigned slot)
+    callback_t* makeCallback(T&& callable, unsigned slot)
     {
         static_assert(sizeof(T) <= slotSize, "class CallbackHelper: The object passed to makeCallback is too large");
-        return new (memoryPool + slot * slotSize) Callback_lambda<T>(std::forward<T>(l)); // please note that placement new does not allocate memory!
+        return new (memoryPool + slot * slotSize) Callback_lambda<T>(std::forward<T>(callable)); // please note that placement new does NOT allocate memory!
     }
-
 
  protected:
     uint8_t memoryPool[slotSize * nrOfSlots]; // since we want to avoid dynamic memory allocation we need to reserve the required memory for the delegates statically
