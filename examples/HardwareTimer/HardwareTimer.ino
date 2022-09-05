ebba4221ed8bd3641e3081682f35d67c2c4f0a7c
luni64 <12611497+luni64@users.noreply.github.com>
2022-09-05 11:58:03 +0200

    Added hardware timer example

---
+++ examples/HardwareTimer/HardwareTimer.ino
@@ -0,0 +1,50 @@
+#include "Arduino.h"
+#include "PIT.h"
+
+/*************************************************************************
+ * This example shows how to use the CallbackHelper library to provide
+ * a std::function like API to a hardware timer (here a T4.x PIT).
+ * Users can attach free functions, functors, lambdas etc to the PIT class
+ *
+ *************************************************************************/
+
+// standard free function which can be attached as callback.
+void freeFunction()
+{
+    Serial.println("Free function called");
+}
+
+// Functors are classes with an overloaded function call operator.
+// Before lambda expressions where available, functors were often used
+// as callbacks which can store state.
+class functor_t
+{
+ public:
+    void operator()()
+    {
+        Serial.printf("Functor called i:%d\n", i);
+    }
+    int i = 17;
+};
+functor_t myFunctor;
+
+PIT myPIT;
+
+void setup()
+{
+    // to test, uncomment one of the examples below
+
+       myPIT.begin(freeFunction, 250'000);                                                // attach a free void(void) function as callback
+    // myPIT.begin(myFunctor, 250'000);                                                   // invoke the function call operator of myFunctor every 250ms
+    // myPIT.begin([] { Serial.println("captureless lambda called"); }, 250'000);         // attach a simple lambda expression
+
+    // int i = 42;
+    // myPIT.begin([i] { Serial.printf("capturing lambda called i:%d\n", i); }, 250'000); // attach a lambda expression, capturing a variable value
+}
+
+void loop()
+{
+    Serial.println("---------------");
+    myFunctor.i = millis(); // change some state of the functor on the fly
+    delay(2000);
+}
---
+++ examples/HardwareTimer/PIT.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "callbackHelper.h"
+
+
+class PIT
+{
+ public:
+    void begin(void (*cb)(), float period)  // normal free function, static member function, captureless lambdas
+    {                                       //
+        callback = cbh.makeCallback(cb, 0); // generate a delegate from the parameters and store it for calling from the ISR
+        initPIT(period);                    // setup PIT module
+    }
+
+    template <class T>
+    void begin(T&& lambda, float period)                         // lambdas, functors etc.
+    {                                                            //
+        callback = cbh.makeCallback(std::forward<T>(lambda), 0); // make a delegate from the parameters and store for calling from the ISR
+        initPIT(period);                                         // setup PIT module
+    }
+
+ protected:
+    static void isr()           // actual ISR
+    {                           //
+        if (PIT_TFLG0)          // did we get a timer event for channel 0
+        {                       //
+            PIT_TFLG0 = 1;      // reset the interrupt flag
+            callback->invoke(); // invoke the stored callback
+        }
+        asm volatile("dsb"); // wait until register changes propagated through the cache to avoid double calls
+    }
+
+    void initPIT(float period)
+    {
+        static bool isInitialized = false;
+
+        if (!isInitialized)
+        {
+            CCM_CCGR1 |= CCM_CCGR1_PIT(CCM_CCGR_ON);
+            PIT_MCR = 1;
+
+            attachInterruptVector(IRQ_PIT, isr);
+            NVIC_ENABLE_IRQ(IRQ_PIT);
+
+            isInitialized = true;
+        }
+
+        PIT_LDVAL0 = period * 24;                   // reload value, period in microseconds, timer clock 24MHz
+        PIT_TCTRL0 = PIT_TCTRL_TEN | PIT_TCTRL_TIE; // enable channel and generate interrupts on overflow
+    }
+
+    CallbackHelper<1> cbh;       // use a callback helper which manages the callback for the PIT (for the sake of simplicity we only use PIT channel 0 here)
+    static callback_t* callback; // in a real implementation this would be a static array of 4 callback_t pointers
+};
+
+callback_t* PIT::callback = nullptr;
