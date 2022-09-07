bad6993c12590ed1b97a11ef821ba6d77f3b726c
luni64 <12611497+luni64@users.noreply.github.com>
2022-09-07 16:32:05 +0200

    Added buffer overflow protection

--- src/CallbackHelper.h
+++ src/CallbackHelper.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Arduino.h"
 #include "callback.h"
+#include <array>
 #include <memory>
 
 // /**
@@ -12,11 +13,11 @@
 //  *   functor:                   total size of class member variables
 //  **/
 
-template <  typename T, size_t nrOfSlots, size_t slotSize=16>
+template <typename T, size_t nrOfSlots, size_t slotSize = 16>
 class CallbackHelper;
 
 template <typename... Args, size_t nrOfSlots, size_t slotSize>
-class CallbackHelper< void(Args...), nrOfSlots, slotSize>
+class CallbackHelper<void(Args...), nrOfSlots, slotSize>
 {
  public:
     using signature  = void(Args...);
@@ -25,9 +26,19 @@ class CallbackHelper< void(Args...), nrOfSlots, slotSize>
     template <typename T>
     callback_t* makeCallback(T&& lbd, size_t slot)
     {
-        return new(memoryPool + slot*slotSize) CallbackImpl<T, Args...>(std::forward<T>(lbd));
+        // make sure user doesn't pass a too large object (e.g. lambda with too much captured parameters) write out of bounds or a slot number larger than nrOfSlots
+        if (slot >= nrOfSlots) return nullptr;
+        static_assert(sizeof(CallbackImpl<T, Args...>) <= slotSize,
+                      "\n****************************************************************************************************"
+                      "\n* CallbackHelper::makeCallback Object too large. Consider increasing slot space for CallbackHelper *"
+                      "\n**************************************************************************************************** ");
+
+        return new (&(memoryPool[slot])) CallbackImpl<T, Args...>(std::forward<T>(lbd));
     }
 
  protected:
-   uint8_t memoryPool[slotSize * nrOfSlots]; // since we want to avoid dynamic memory allocation we need to reserve the required memory for the delegates statically
+    //  std::array<uint8_t, slotSize> slotPool;
+    //  std::array<std::array<uint8_t, slotSize>,nrOfSlots> memoryPool;
+
+    uint8_t memoryPool[nrOfSlots][slotSize]; // since we want to avoid dynamic memory allocation we need to reserve the required memory for the delegates statically
 };
